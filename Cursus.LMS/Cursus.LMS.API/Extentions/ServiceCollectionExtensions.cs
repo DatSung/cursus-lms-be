7b97f453425d3557a5ad2ed42e17b3ba6d7daa04
DatSung <datsung.dev@gmail.com>
2024-06-06 22:59:10 +0700

    Add repository for Student, PaymentCard and Instructor

--- Cursus.LMS/Cursus.LMS.API/Extentions/ServiceCollectionExtensions.cs
+++ Cursus.LMS/Cursus.LMS.API/Extentions/ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using Cursus.LMS.DataAccess.Context;
+using Cursus.LMS.DataAccess.IRepository;
+using Cursus.LMS.DataAccess.Repository;
 using Cursus.LMS.Model.Domain;
 using Cursus.LMS.Service.IService;
 using Cursus.LMS.Service.Service;
@@ -10,6 +12,8 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection RegisterServices(this IServiceCollection services)
     {
+        // Registering IUnitOfWork with its implementation UnitOfWork
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
         // Registering IBaseService with its implementation BaseService
         services.AddScoped<IBaseService, BaseService>();
         // Registering IAuthService with its implementation AuthService
---
+++ Cursus.LMS/Cursus.LMS.DataAccess/IRepository/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using Cursus.LMS.DataAccess.IRepository;
+
+namespace Cursus.LMS.DataAccess.IRepository;
+
+public interface IUnitOfWork
+{
+    IStudentRepository StudentRepository { get; }
+
+    Task SaveAsync();
+}
