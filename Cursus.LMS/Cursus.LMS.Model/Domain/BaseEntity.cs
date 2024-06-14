07975b3f6e457b8ceebd6933f6f1684f78484900
DatSung <datsung.dev@gmail.com>
2024-06-14 08:39:59 +0700

    Modify: add more properties in table Category

---
+++ Cursus.LMS/Cursus.LMS.Model/Domain/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Cursus.LMS.Model.Domain;
+
+public class BaseEntity<CID, UID>
+{
+    public CID CreateBy { get; set; }
+    public DateTime CreateTime { get; set; }
+    public UID UpdateBy { get; set; }
+    public DateTime UpdateTime { get; set; }
+}
--- Cursus.LMS/Cursus.LMS.Model/Domain/Category.cs
+++ Cursus.LMS/Cursus.LMS.Model/Domain/Category.cs
@@ -1,9 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Cursus.LMS.Model.Domain;
 
-public class Category
+public class Category : BaseEntity<string, string>
 {
     [Key] public Guid Id { get; set; }
     public string Name { get; set; }
+    public string? Description { get; set; }
+    public Guid? ParentId { get; set; }
+    [ForeignKey("ParentId")] public Category ParentCategory { get; set; }
+    public int Status { get; set; } = 0; // 0 mean New, 1 mean Active, 2 mean Deactivated.
 }
