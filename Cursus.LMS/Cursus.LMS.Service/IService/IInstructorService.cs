5d45979c99e6dcce2cd81ca3107ae49fd1fb26fa
DatSung <datsung.dev@gmail.com>
2024-06-17 11:18:24 +0700

    prepare for instructor

---
+++ Cursus.LMS/Cursus.LMS.Service/IService/IInstructorService.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Claims;
+using Cursus.LMS.Model.DTO;
+
+namespace Cursus.LMS.Service.IService;
+
+public interface IInstructorService
+{
+    Task<ResponseDTO> GetAll
+    (
+        ClaimsPrincipal User,
+        string? filterOn,
+        string? filterQuery,
+        string? sortBy,
+        bool? isAscending,
+        int pageNumber,
+        int pageSize
+    );
+
+    Task<ResponseDTO> GetById(Guid id);
+    Task<ResponseDTO> UpdateById(Guid id, UpdateInstructorDTO updateInstructorDto);
+    Task<ResponseDTO> AcceptInstructor(ClaimsPrincipal User, Guid id);
+    Task<ResponseDTO> RejectInstructor(ClaimsPrincipal User, Guid id);
+}
