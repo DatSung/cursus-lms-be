bb1ae675be735608dc2d993afdb47aa120af886e
DatSung <datsung.dev@gmail.com>
2024-06-17 18:17:31 +0700

    modified database: remove reject in instructor

---
+++ Cursus.LMS/Cursus.LMS.Model/DTO/InstructorInfoLiteDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Cursus.LMS.Model.DTO;
+
+public class InstructorInforLiteDTO
+{
+    public string? FullName { get; set; }
+    public string? Email { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Gender { get; set; }
+    public DateTime BirthDate { get; set; }
+}
