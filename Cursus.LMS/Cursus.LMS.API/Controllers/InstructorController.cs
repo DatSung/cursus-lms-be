5bc73178b1c4f83f1e88010f2d76ffd006ff9ed2
DatSung <datsung.dev@gmail.com>
2024-06-17 09:32:21 +0700

    add: instructor controller

---
+++ Cursus.LMS/Cursus.LMS.API/Controllers/InstructorController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cursus.LMS.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InstructorController : ControllerBase
+    {
+    }
+}
