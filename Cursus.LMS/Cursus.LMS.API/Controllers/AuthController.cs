09e410611b8a95156fce2d2953ccdfe81a3a8ba8
DatSung <datsung.dev@gmail.com>
2024-06-26 08:39:35 +0700

    modify send email url

--- Cursus.LMS/Cursus.LMS.API/Controllers/AuthController.cs
+++ Cursus.LMS/Cursus.LMS.API/Controllers/AuthController.cs
@@ -235,7 +235,7 @@ namespace Cursus.LMS.API.Controllers
             var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
 
             var confirmationLink =
-                $"http://localhost:30475/sign-in/verify-email?userId={user.Id}&token={Uri.EscapeDataString(token)}";
+                $"http://localhost:30475/user/sign-in/verify-email?userId={user.Id}&token={Uri.EscapeDataString(token)}";
 
             var responseDto = await _authService.SendVerifyEmail(user.Email, confirmationLink);
 
