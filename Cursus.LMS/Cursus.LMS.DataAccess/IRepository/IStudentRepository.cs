5f05cf899321bb74d50961278abb98ec0047e79d
dotrong-code <dotrong159357@gmail.com>
2024-07-12 14:16:46 +0700

    Done_Update profile

--- Cursus.LMS/Cursus.LMS.DataAccess/IRepository/IStudentRepository.cs
+++ Cursus.LMS/Cursus.LMS.DataAccess/IRepository/IStudentRepository.cs
@@ -7,4 +7,5 @@ public interface IStudentRepository : IRepository<Student>
     void Update(Student student);
     void UpdateRange(IEnumerable<Student> students);
     Task<Student> AddAsync(Student student);
+    Task<Student?> GetByUserId(string id);
 }
