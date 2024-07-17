d520115d051e7a5f472038b0a93db5b80b6ce140
DatSung <datsung.dev@gmail.com>
2024-07-17 15:28:47 +0700

    feat: update balance by order id

--- Cursus.LMS/Cursus.LMS.Service/IService/ICartService.cs
+++ Cursus.LMS/Cursus.LMS.Service/IService/ICartService.cs
@@ -8,4 +8,5 @@ public interface ICartService
     Task<ResponseDTO> GetCart(ClaimsPrincipal User);
     Task<ResponseDTO> AddToCart(ClaimsPrincipal User, AddToCartDTO addToCartDto);
     Task<ResponseDTO> RemoveFromCart(ClaimsPrincipal User, Guid courseId);
+    Task<ResponseDTO> Checkout(ClaimsPrincipal User);
 }
--- Cursus.LMS/Cursus.LMS.Service/IService/IPaymentService.cs
+++ Cursus.LMS/Cursus.LMS.Service/IService/IPaymentService.cs
@@ -1,6 +1,8 @@
-﻿namespace Cursus.LMS.Service.IService;
+﻿using Cursus.LMS.Model.DTO;
+
+namespace Cursus.LMS.Service.IService;
 
 public interface IPaymentService
 {
-    
+    Task<ResponseDTO> UpdateBalanceByOrderId(Guid orderHeaderId);
 }
