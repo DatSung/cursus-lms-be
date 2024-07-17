b51f59974d2dd5ab524d472fa1dec1ee10295187
DatSung <datsung.dev@gmail.com>
2024-07-17 10:03:59 +0700

    add service and iservice for balance and transaction

---
+++ Cursus.LMS/Cursus.LMS.Service/Service/BalanceService.cs
@@ -0,0 +1,12 @@
+﻿using Cursus.LMS.Model.DTO;
+using Cursus.LMS.Service.IService;
+
+namespace Cursus.LMS.Service.Service;
+
+public class BalanceService : IBalanceService
+{
+    public Task<ResponseDTO> UpSertBalance(UpSertBalanceDTO upSertBalanceDto)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Cursus.LMS/Cursus.LMS.Service/Service/TransactionService.cs
@@ -0,0 +1,12 @@
+﻿using Cursus.LMS.Model.DTO;
+using Cursus.LMS.Service.IService;
+
+namespace Cursus.LMS.Service.Service;
+
+public class TransactionService : ITransactionService
+{
+    public Task<ResponseDTO> CreateTransaction(CreateTransactionDTO createTransactionDto)
+    {
+        throw new NotImplementedException();
+    }
+}
