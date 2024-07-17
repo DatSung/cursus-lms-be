403ce4437d63579996683ab5eddbc0f1b5dfaeeb
DatSung <datsung.dev@gmail.com>
2024-07-17 11:06:03 +0700

    modified database: remove remain colume in table transaction

---
+++ Cursus.LMS/Cursus.LMS.DataAccess/Migrations/20240717040425_RemoveRemainInTableTransaction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Cursus.LMS.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveRemainInTableTransaction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.DropColumn(
+                name: "Remain",
+                table: "Transactions");
+
+          
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+            migrationBuilder.AddColumn<double>(
+                name: "Remain",
+                table: "Transactions",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+            
+        }
+    }
+}
