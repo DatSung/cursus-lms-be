e655c7075d495ac9e4cf16f023d8628d83e0b3b7
DatSung <datsung.dev@gmail.com>
2024-07-17 09:16:49 +0700

    add table balances

---
+++ Cursus.LMS/Cursus.LMS.Model/Domain/Balance.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cursus.LMS.Model.Domain;
+
+public class Balance
+{
+    [Key] public Guid InstructorId { get; set; }
+
+    public int AvailableBalance { get; set; }
+
+    public int PendingBalance { get; set; }
+
+    public string Currency { get; set; }
+
+    public DateTime UpdatedTime { get; set; }
+
+    [ForeignKey("InstructorId")] public virtual Instructor? Instructor { get; set; }
+}
