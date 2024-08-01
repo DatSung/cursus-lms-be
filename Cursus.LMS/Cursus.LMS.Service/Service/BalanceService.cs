ebbd05d349afdf051b18a9a05a021c933e1b90d2
DatSung <datsung.dev@gmail.com>
2024-08-01 21:54:03 +0700

    feat: get balance for user

--- Cursus.LMS/Cursus.LMS.Service/Service/BalanceService.cs
+++ Cursus.LMS/Cursus.LMS.Service/Service/BalanceService.cs
@@ -1,23 +1,73 @@
 ﻿using System.Security.Claims;
+using AutoMapper;
 using Cursus.LMS.DataAccess.IRepository;
 using Cursus.LMS.Model.Domain;
 using Cursus.LMS.Model.DTO;
 using Cursus.LMS.Service.IService;
+using Cursus.LMS.Utility.Constants;
 
 namespace Cursus.LMS.Service.Service;
 
 public class BalanceService : IBalanceService
 {
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
 
-    public BalanceService(IUnitOfWork unitOfWork)
+    public BalanceService(IUnitOfWork unitOfWork, IMapper mapper)
     {
         _unitOfWork = unitOfWork;
+        _mapper = mapper;
     }
 
-    public Task<ResponseDTO> GetBalance(ClaimsPrincipal User, string? userId)
+    public async Task<ResponseDTO> GetBalance(ClaimsPrincipal User, string? userId)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var role = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Role)!.Value;
+            var balance = new Balance();
+
+            if (role.Contains(StaticUserRoles.Admin))
+            {
+                if (userId is null)
+                {
+                    return new ResponseDTO()
+                    {
+                        IsSuccess = false,
+                        Message = "User was not found",
+                        StatusCode = 404,
+                        Result = null
+                    };
+                }
+
+                balance = await _unitOfWork.BalanceRepository.GetAsync(x => x.UserId == userId);
+            }
+
+            if (role.Contains(StaticUserRoles.Instructor))
+            {
+                userId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)!.Value;
+                balance = await _unitOfWork.BalanceRepository.GetAsync(x => x.UserId == userId);
+            }
+
+            var getBalanceDto = _mapper.Map<GetBalanceDTO>(balance);
+
+            return new ResponseDTO()
+            {
+                IsSuccess = true,
+                Message = "Get balance successfully",
+                StatusCode = 200,
+                Result = getBalanceDto
+            };
+        }
+        catch (Exception e)
+        {
+            return new ResponseDTO()
+            {
+                IsSuccess = false,
+                Message = e.Message,
+                Result = null,
+                StatusCode = 500
+            };
+        }
     }
 
     public async Task<ResponseDTO> UpsertBalance(UpsertBalanceDTO upsertBalanceDto)
