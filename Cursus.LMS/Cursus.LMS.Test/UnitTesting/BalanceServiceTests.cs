ce3acb655ca94307398bd05901b4cf0b2dc7428f
dotrong-code <dotrong159357@gmail.com>
2024-08-07 18:09:05 +0700

    Final_Unit_Test

--- Cursus.LMS/Cursus.LMS.Test/UnitTesting/BalanceServiceTests.cs
+++ Cursus.LMS/Cursus.LMS.Test/UnitTesting/BalanceServiceTests.cs
@@ -1,84 +1,84 @@
-﻿//using System;
-//using System.Threading.Tasks;
-//using Moq;
-//using Xunit;
-//using Cursus.LMS.DataAccess.IRepository;
-//using Cursus.LMS.Model.Domain;
-//using Cursus.LMS.Model.DTO;
-//using Cursus.LMS.Service.Service;
-//using Cursus.LMS.Service.IService;
-//using System.Linq.Expressions;
+﻿using System;
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+using Cursus.LMS.DataAccess.IRepository;
+using Cursus.LMS.Model.Domain;
+using Cursus.LMS.Model.DTO;
+using Cursus.LMS.Service.Service;
+using Cursus.LMS.Service.IService;
+using System.Linq.Expressions;
 
-//namespace Cursus.LMS.Service.Tests
-//{
-//    public class BalanceServiceTests
-//    {
-//        private readonly Mock<IUnitOfWork> _mockUnitOfWork;
-//        private readonly BalanceService _balanceService;
+namespace Cursus.LMS.Service.Tests
+{
+    public class BalanceServiceTests
+    {
+        private readonly Mock<IUnitOfWork> _mockUnitOfWork;
+        private readonly BalanceService _balanceService;
 
-//        public BalanceServiceTests()
-//        {
-//            _mockUnitOfWork = new Mock<IUnitOfWork>();
-//            _balanceService = new BalanceService(_mockUnitOfWork.Object);
-//        }
+        public BalanceServiceTests()
+        {
+            _mockUnitOfWork = new Mock<IUnitOfWork>();
+            _balanceService = new BalanceService(_mockUnitOfWork.Object);
+        }
 
-//        [Fact]
-//        public async Task UpsertBalance_ShouldAddNewBalance_WhenBalanceDoesNotExist()
-//        {
-//            // Arrange
-//            var upsertBalanceDto = new UpsertBalanceDTO
-//            {
-//                UserId = "upsertBalance ID",
-//                TotalBalance = 100,
-//                AvailableBalance = 50,
-//                PayoutBalance = 30
-//            };
+        [Fact]
+        public async Task UpsertBalance_ShouldAddNewBalance_WhenBalanceDoesNotExist()
+        {
+            // Arrange
+            var upsertBalanceDto = new UpsertBalanceDTO
+            {
+                UserId = "upsertBalance ID",
+                TotalBalance = 100,
+                AvailableBalance = 50,
+                PayoutBalance = 30
+            };
 
-//            _mockUnitOfWork.Setup(u => u.BalanceRepository.GetAsync(It.IsAny<Expression<Func<Balance, bool>>>(), It.IsAny<string>()))
-//                .ReturnsAsync((Balance)null);
+            _mockUnitOfWork.Setup(u => u.BalanceRepository.GetAsync(It.IsAny<Expression<Func<Balance, bool>>>(), It.IsAny<string>()))
+                .ReturnsAsync((Balance)null);
 
-//            _mockUnitOfWork.Setup(u => u.BalanceRepository.AddAsync(It.IsAny<Balance>()))
-//                .Returns(Task.CompletedTask);
+            _mockUnitOfWork.Setup(u => u.BalanceRepository.AddAsync(It.IsAny<Balance>()))
+                .Returns(Task.CompletedTask);
 
-//            _mockUnitOfWork.Setup(u => u.SaveAsync())
-//                .ReturnsAsync(1);
+            _mockUnitOfWork.Setup(u => u.SaveAsync())
+                .ReturnsAsync(1);
 
-//            // Act
-//            var result = await _balanceService.UpsertBalance(upsertBalanceDto);
+            // Act
+            var result = await _balanceService.UpsertBalance(upsertBalanceDto);
 
-//            // Assert
-//            Assert.True(result.IsSuccess);
-//            Assert.Equal(200, result.StatusCode);
-//            _mockUnitOfWork.Verify(u => u.BalanceRepository.AddAsync(It.IsAny<Balance>()), Times.Once);
-//            _mockUnitOfWork.Verify(u => u.SaveAsync(), Times.Once);
-//        }
-
-        
+            // Assert
+            Assert.True(result.IsSuccess);
+            Assert.Equal(200, result.StatusCode);
+            _mockUnitOfWork.Verify(u => u.BalanceRepository.AddAsync(It.IsAny<Balance>()), Times.Once);
+            _mockUnitOfWork.Verify(u => u.SaveAsync(), Times.Once);
+        }
 
 
 
-//        [Fact]
-//        public async Task UpsertBalance_ShouldReturnError_WhenExceptionThrown()
-//        {
-//            // Arrange
-//            var upsertBalanceDto = new UpsertBalanceDTO
-//            {
-//                UserId = "upsertBalance ID",
-//                TotalBalance = 100,
-//                AvailableBalance = 50,
-//                PayoutBalance = 30
-//            };
 
-//            _mockUnitOfWork.Setup(u => u.BalanceRepository.GetAsync(It.IsAny<Expression<Func<Balance, bool>>>(), It.IsAny<string>()))
-//                .ThrowsAsync(new Exception("Database error"));
 
-//            // Act
-//            var result = await _balanceService.UpsertBalance(upsertBalanceDto);
+        [Fact]
+        public async Task UpsertBalance_ShouldReturnError_WhenExceptionThrown()
+        {
+            // Arrange
+            var upsertBalanceDto = new UpsertBalanceDTO
+            {
+                UserId = "upsertBalance ID",
+                TotalBalance = 100,
+                AvailableBalance = 50,
+                PayoutBalance = 30
+            };
 
-//            // Assert
-//            Assert.False(result.IsSuccess);
-//            Assert.Equal(500, result.StatusCode);
-//            Assert.Equal("Database error", result.Message);
-//        }
-//    }
-//}
+            _mockUnitOfWork.Setup(u => u.BalanceRepository.GetAsync(It.IsAny<Expression<Func<Balance, bool>>>(), It.IsAny<string>()))
+                .ThrowsAsync(new Exception("Database error"));
+
+            // Act
+            var result = await _balanceService.UpsertBalance(upsertBalanceDto);
+
+            // Assert
+            Assert.False(result.IsSuccess);
+            Assert.Equal(500, result.StatusCode);
+            Assert.Equal("Database error", result.Message);
+        }
+    }
+}
--- Cursus.LMS/Cursus.LMS.Test/UnitTesting/CompanyServiceTests.cs
+++ Cursus.LMS/Cursus.LMS.Test/UnitTesting/CompanyServiceTests.cs
@@ -1,139 +1,139 @@
-﻿//using AutoMapper;
-//using Cursus.LMS.DataAccess.IRepository;
-//using Cursus.LMS.Model.Domain;
-//using Cursus.LMS.Model.DTO;
-//using Cursus.LMS.Service.Service;
-//using Moq;
-//using System;
-//using System.Linq.Expressions;
-//using System.Threading.Tasks;
-//using Xunit;
+﻿using AutoMapper;
+using Cursus.LMS.DataAccess.IRepository;
+using Cursus.LMS.Model.Domain;
+using Cursus.LMS.Model.DTO;
+using Cursus.LMS.Service.Service;
+using Moq;
+using System;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Xunit;
 
-//namespace Cursus.LMS.Test.UnitTesting
-//{
-//    public class CompanyServiceTests
-//    {
-//        private readonly Mock<IUnitOfWork> _mockUnitOfWork;
-//        private readonly Mock<IMapper> _mockMapper;
-//        private readonly CompanyService _companyService;
+namespace Cursus.LMS.Test.UnitTesting
+{
+    public class CompanyServiceTests
+    {
+        private readonly Mock<IUnitOfWork> _mockUnitOfWork;
+        private readonly Mock<IMapper> _mockMapper;
+        private readonly CompanyService _companyService;
 
-//        public CompanyServiceTests()
-//        {
-//            _mockUnitOfWork = new Mock<IUnitOfWork>();
-//            _mockMapper = new Mock<IMapper>();
-//            _companyService = new CompanyService(_mockUnitOfWork.Object, _mockMapper.Object);
-//        }
+        public CompanyServiceTests()
+        {
+            _mockUnitOfWork = new Mock<IUnitOfWork>();
+            _mockMapper = new Mock<IMapper>();
+            _companyService = new CompanyService(_mockUnitOfWork.Object, _mockMapper.Object);
+        }
 
-//        [Fact]
-//        public async Task GetCompany_ShouldReturnCompany_WhenCompanyExists()
-//        {
-//            // Arrange
-//            var company = new Company { Id = Guid.NewGuid() };
+        [Fact]
+        public async Task GetCompany_ShouldReturnCompany_WhenCompanyExists()
+        {
+            // Arrange
+            var company = new Company { Id = Guid.NewGuid() };
 
-//            _mockUnitOfWork.Setup(u => u.CompanyRepository.GetAsync(It.IsAny<Expression<Func<Company, bool>>>(), It.IsAny<string>()))
-//                .ReturnsAsync(company);
+            _mockUnitOfWork.Setup(u => u.CompanyRepository.GetAsync(It.IsAny<Expression<Func<Company, bool>>>(), It.IsAny<string>()))
+                .ReturnsAsync(company);
 
-//            // Act
-//            var result = await _companyService.GetCompany();
+            // Act
+            var result = await _companyService.GetCompany();
 
-//            // Assert
-//            Assert.True(result.IsSuccess);
-//            Assert.Equal(200, result.StatusCode);
-//            Assert.Equal("Get company successfully", result.Message);
-//            Assert.Equal(company, result.Result);
-//        }
+            // Assert
+            Assert.True(result.IsSuccess);
+            Assert.Equal(200, result.StatusCode);
+            Assert.Equal("Get company successfully", result.Message);
+            Assert.Equal(company, result.Result);
+        }
 
-//        [Fact]
-//        public async Task GetCompany_ShouldReturnNotFound_WhenCompanyDoesNotExist()
-//        {
-//            // Arrange
-//            _mockUnitOfWork.Setup(u => u.CompanyRepository.GetAsync(It.IsAny<Expression<Func<Company, bool>>>(), It.IsAny<string>()))
-//                .ReturnsAsync((Company)null);
+        [Fact]
+        public async Task GetCompany_ShouldReturnNotFound_WhenCompanyDoesNotExist()
+        {
+            // Arrange
+            _mockUnitOfWork.Setup(u => u.CompanyRepository.GetAsync(It.IsAny<Expression<Func<Company, bool>>>(), It.IsAny<string>()))
+                .ReturnsAsync((Company)null);
 
-//            // Act
-//            var result = await _companyService.GetCompany();
+            // Act
+            var result = await _companyService.GetCompany();
 
-//            // Assert
-//            Assert.False(result.IsSuccess);
-//            Assert.Equal(404, result.StatusCode);
-//            Assert.Equal("Company not found", result.Message);
-//        }
+            // Assert
+            Assert.False(result.IsSuccess);
+            Assert.Equal(404, result.StatusCode);
+            Assert.Equal("Company not found", result.Message);
+        }
 
-//        [Fact]
-//        public async Task GetCompany_ShouldReturnInternalServerError_WhenExceptionIsThrown()
-//        {
-//            // Arrange
-//            _mockUnitOfWork.Setup(u => u.CompanyRepository.GetAsync(It.IsAny<Expression<Func<Company, bool>>>(), It.IsAny<string>()))
-//                .ThrowsAsync(new Exception("Database error"));
+        [Fact]
+        public async Task GetCompany_ShouldReturnInternalServerError_WhenExceptionIsThrown()
+        {
+            // Arrange
+            _mockUnitOfWork.Setup(u => u.CompanyRepository.GetAsync(It.IsAny<Expression<Func<Company, bool>>>(), It.IsAny<string>()))
+                .ThrowsAsync(new Exception("Database error"));
 
-//            // Act
-//            var result = await _companyService.GetCompany();
+            // Act
+            var result = await _companyService.GetCompany();
 
-//            // Assert
-//            Assert.False(result.IsSuccess);
-//            Assert.Equal(500, result.StatusCode);
-//            Assert.Equal("Database error", result.Message);
-//        }
+            // Assert
+            Assert.False(result.IsSuccess);
+            Assert.Equal(500, result.StatusCode);
+            Assert.Equal("Database error", result.Message);
+        }
 
-//        [Fact]
-//        public async Task UpdateCompany_ShouldReturnSuccess_WhenCompanyIsUpdated()
-//        {
-//            // Arrange
-//            var companyDto = new UpdateCompanyDTO { Id = Guid.NewGuid() };
-//            var company = new Company { Id = companyDto.Id };
+        [Fact]
+        public async Task UpdateCompany_ShouldReturnSuccess_WhenCompanyIsUpdated()
+        {
+            // Arrange
+            var companyDto = new UpdateCompanyDTO { Id = Guid.NewGuid() };
+            var company = new Company { Id = companyDto.Id };
 
-//            _mockUnitOfWork.Setup(u => u.CompanyRepository.GetAsync(It.IsAny<Expression<Func<Company, bool>>>(), It.IsAny<string>()))
-//                .ReturnsAsync(company);
+            _mockUnitOfWork.Setup(u => u.CompanyRepository.GetAsync(It.IsAny<Expression<Func<Company, bool>>>(), It.IsAny<string>()))
+                .ReturnsAsync(company);
 
-//            // Act
-//            var result = await _companyService.UpdateCompany(companyDto);
+            // Act
+            var result = await _companyService.UpdateCompany(companyDto);
 
-//            // Assert
-//            Assert.True(result.IsSuccess);
-//            Assert.Equal(200, result.StatusCode);
-//            Assert.Equal("Company updated successfully", result.Message);
+            // Assert
+            Assert.True(result.IsSuccess);
+            Assert.Equal(200, result.StatusCode);
+            Assert.Equal("Company updated successfully", result.Message);
 
-//            _mockUnitOfWork.Verify(u => u.CompanyRepository.Update(It.IsAny<Company>()), Times.Once);
-//            _mockUnitOfWork.Verify(u => u.SaveAsync(), Times.Once);
-//        }
+            _mockUnitOfWork.Verify(u => u.CompanyRepository.Update(It.IsAny<Company>()), Times.Once);
+            _mockUnitOfWork.Verify(u => u.SaveAsync(), Times.Once);
+        }
 
-//        [Fact]
-//        public async Task UpdateCompany_ShouldReturnNotFound_WhenCompanyDoesNotExist()
-//        {
-//            // Arrange
-//            var companyDto = new UpdateCompanyDTO { Id = Guid.NewGuid() };
+        [Fact]
+        public async Task UpdateCompany_ShouldReturnNotFound_WhenCompanyDoesNotExist()
+        {
+            // Arrange
+            var companyDto = new UpdateCompanyDTO { Id = Guid.NewGuid() };
 
-//            _mockUnitOfWork.Setup(u => u.CompanyRepository.GetAsync(It.IsAny<Expression<Func<Company, bool>>>(), It.IsAny<string>()))
-//                .ReturnsAsync((Company)null);
+            _mockUnitOfWork.Setup(u => u.CompanyRepository.GetAsync(It.IsAny<Expression<Func<Company, bool>>>(), It.IsAny<string>()))
+                .ReturnsAsync((Company)null);
 
-//            // Act
-//            var result = await _companyService.UpdateCompany(companyDto);
+            // Act
+            var result = await _companyService.UpdateCompany(companyDto);
 
-//            // Assert
-//            Assert.False(result.IsSuccess);
-//            Assert.Equal(404, result.StatusCode);
-//            Assert.Equal("Company not found", result.Message);
-//        }
+            // Assert
+            Assert.False(result.IsSuccess);
+            Assert.Equal(404, result.StatusCode);
+            Assert.Equal("Company not found", result.Message);
+        }
 
-//        [Fact]
-//        public async Task UpdateCompany_ShouldReturnInternalServerError_WhenExceptionIsThrown()
-//        {
-//            // Arrange
-//            var companyDto = new UpdateCompanyDTO { Id = Guid.NewGuid() };
-//            var company = new Company { Id = companyDto.Id };
+        [Fact]
+        public async Task UpdateCompany_ShouldReturnInternalServerError_WhenExceptionIsThrown()
+        {
+            // Arrange
+            var companyDto = new UpdateCompanyDTO { Id = Guid.NewGuid() };
+            var company = new Company { Id = companyDto.Id };
 
-//            _mockUnitOfWork.Setup(u => u.CompanyRepository.GetAsync(It.IsAny<Expression<Func<Company, bool>>>(), It.IsAny<string>()))
-//                .ReturnsAsync(company);
-//            _mockUnitOfWork.Setup(u => u.SaveAsync())
-//                .ThrowsAsync(new Exception("Database error"));
+            _mockUnitOfWork.Setup(u => u.CompanyRepository.GetAsync(It.IsAny<Expression<Func<Company, bool>>>(), It.IsAny<string>()))
+                .ReturnsAsync(company);
+            _mockUnitOfWork.Setup(u => u.SaveAsync())
+                .ThrowsAsync(new Exception("Database error"));
 
-//            // Act
-//            var result = await _companyService.UpdateCompany(companyDto);
+            // Act
+            var result = await _companyService.UpdateCompany(companyDto);
 
-//            // Assert
-//            Assert.False(result.IsSuccess);
-//            Assert.Equal(500, result.StatusCode);
-//            Assert.Equal("Database error", result.Message);
-//        }
-//    }
-//}
+            // Assert
+            Assert.False(result.IsSuccess);
+            Assert.Equal(500, result.StatusCode);
+            Assert.Equal("Database error", result.Message);
+        }
+    }
+}
